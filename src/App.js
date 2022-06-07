3d290a348a7dba92b8b0c340e8394e9063cd5235
Mariah Morales <mjustine622@gmail.com>
2022-06-07 15:28:32 -0400

    made SearchBox.js filed and created state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, {Component} from 'react'
 import CardList from './CardList';
+import SearchBox from './SearchBox'
 import { robots } from './robots';
 
+// state is an object; changeable;
+//props are things that come out of state.
 
-const App = () => {
-    return (
-        <CardList robots={robots}/>
 
-    )
+
+
+class App extends Component{
+    constructor(){
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+    render(){
+        return (
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox/>
+                <CardList robots={robots}/>
+            </div>
+
+        )
+    }
 }
 
 export default App
