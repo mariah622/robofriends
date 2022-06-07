921d99dd0f118f4d4b9c33cb504aad591e33164c
Mariah Morales <mjustine622@gmail.com>
2022-06-06 21:58:20 -0400

    started project, started card component

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img src='' alt='robots'></img>
+            <div>
+                <h2>Name</h2>
+                <p>Email</p>
+            </div>
+        </div>
+    )
+}
+export default Card
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import Card from './Card'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+    <Card/>
+    <Card/>
+    <Card/>
+    </div>
+
   </React.StrictMode>
 );
 
